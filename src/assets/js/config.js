0807eccdd9d77b189a27037ddf988a3bc6a193cb
diego <goethalsdiego@gmail.com>
2022-11-02 21:17:38 +0100

    Got rid of useless code and changed apiurl for test purposes

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -4,5 +4,5 @@ const _config = {
     groupnumber: '24',
     gamePrefix: "group24",
     errorHandlerSelector: '.errormessages p',
-    getAPIUrl: function() { return `https://project-i.ti.howest.be/monopoly-${this.groupnumber}/api`;}
+    getAPIUrl: function() { return `http://localhost:8080`;}
 }
