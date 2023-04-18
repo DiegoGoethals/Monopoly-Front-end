9529fb8acf21755c47221f9ad36676eac5bed9f2
diego <goethalsdiego@gmail.com>
2023-04-18 14:13:24 +0200

    Added hidden button to delete all games on the server

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,7 @@
 
 function initIndex() {
     document.getElementById("username").addEventListener("input", enable);
+    document.getElementById("deleteGames").addEventListener("click", deleteGames);
 }
 
 function enable() {
@@ -15,4 +16,10 @@ function enable() {
         const $button = document.getElementById("play");
         $button.disabled = true;
     }
+}
+
+function deleteGames() {
+    fetchFromServer("/games", "DELETE").then(games => {
+        console.log("Games deleted", games);
+    });
 }
